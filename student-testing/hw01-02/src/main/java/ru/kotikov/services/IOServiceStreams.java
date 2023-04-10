2507ecdbcb2667c441ec54a4b5448ee40c206e11
Kirill Kotikov <kirill.kotikov@rt.ru>
2023-04-10 17:16:29 +0700

    hw01-02: Removed config and changed IOServiceStreams.

--- student-testing/hw01-02/src/main/java/ru/kotikov/services/IOServiceStreams.java
+++ student-testing/hw01-02/src/main/java/ru/kotikov/services/IOServiceStreams.java
@@ -1,14 +1,22 @@
 package ru.kotikov.services;
 
+import org.springframework.stereotype.Service;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
+@Service
 public class IOServiceStreams implements IOService {
     private final PrintStream output;
 
     private final Scanner input;
+
+    public IOServiceStreams() {
+        output = new PrintStream(System.out);
+        input = new Scanner(System.in);
+    }
 
     public IOServiceStreams(OutputStream outputStream, InputStream inputStream) {
         output = new PrintStream(outputStream);
